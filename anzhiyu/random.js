c81e5c682b5fe44d526fe41f69110033fa660dc1
Hanser-Chan <852627653cjj@gmail.com>
2024-09-24 23:01:58 +0800

    Site updated: 2024-09-24 23:01:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/24/hello-world/","2024/09/24/algo1/"];function toRandomPost(){
+var posts=["2024/09/24/algo1/","2024/09/24/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

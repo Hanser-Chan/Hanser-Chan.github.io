e7241615f93478b1bc21bd79c00dbd6e3bac606c
HanserChan <852627653cjj@gmail.com>
2024-09-28 12:37:08 +0800

    Site updated: 2024-09-28 12:37:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/01/categories/Code/ff/","2024/09/26/categories/University/join github edu/"];function toRandomPost(){
+var posts=["2024/05/01/categories/Code/ff/","2024/09/26/categories/University/join github edu/","2024/09/28/categories/README_副本/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

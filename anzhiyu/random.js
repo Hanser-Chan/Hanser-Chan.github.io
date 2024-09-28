837f3c72067ada81a287645f63299de9811f5dad
HanserChan <852627653cjj@gmail.com>
2024-09-28 12:59:18 +0800

    sync

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2024/05/01/categories/Code/ff/","2024/09/26/categories/University/join github edu/","2024/09/28/categories/README_副本/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };

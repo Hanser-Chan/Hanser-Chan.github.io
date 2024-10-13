71c7a884eb263d31d7cff2c967bb9db59a9805ec
HanserChan <852627653cjj@gmail.com>
2024-10-13 21:00:10 +0800

    Site updated: 2024-10-13 21:00:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["docs/FFmpeg_so/","docs/follow_invite/","docs/hexo_rss/","docs/github_edu/","docs/xiaomi_camp/","docs/target/"];function toRandomPost(){
+var posts=["docs/book_duoyu/","docs/xiaomi_camp/","docs/github_edu/","docs/FFmpeg_so/","docs/follow_invite/","docs/hexo_rss/","docs/target/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

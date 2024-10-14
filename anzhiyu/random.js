c3a361302e2028e56e1d370038541b3df4963407
HanserChan <852627653cjj@gmail.com>
2024-10-14 20:26:37 +0800

    Site updated: 2024-10-14 20:26:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["docs/book_duoyu/","docs/FFmpeg_so/","docs/follow_invite/","docs/hexo_rss/","docs/target/","docs/github_edu/","docs/xiaomi_camp/"];function toRandomPost(){
+var posts=["docs/FFmpeg_so/","docs/book_duoyu/","docs/csky408_algo/","docs/hexo_rss/","docs/follow_invite/","docs/target/","docs/github_edu/","docs/xiaomi_camp/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
